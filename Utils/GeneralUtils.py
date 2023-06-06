b3b40b07adf034c3cf9dc29f5293a2ab0e4054a3
rordaz <rafael.ordaz@gmail.com>
2023-06-06 16:35:14 -0500

    Added preferences for Dev Env.

--- Utils/GeneralUtils.py
+++ Utils/GeneralUtils.py
@@ -1,9 +1,21 @@
+import os
 import json
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# Get the value of the ENV variable
+env = os.getenv('ENV')
  
 def ReadPreferences():
+
+    if env == 'development':
+        json_file_path = 'preferences.dev.json'
+    else:
+        json_file_path = 'preferences.json'
     prefDict = []
     try:
-        with open('preferences.json') as preferences:
+        with open(json_file_path) as preferences:
             data = json.load(preferences)
 
         for repository in data["repositories"]:
