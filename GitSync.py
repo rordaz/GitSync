5e647564cf672c4e24fc43d882518251428a4463
rordaz <rafael.ordaz@gmail.com>
2023-06-08 20:50:25 -0500

    Clean up

--- GitSync.py
+++ GitSync.py
@@ -11,15 +11,14 @@ applicationSettings = gu.ReadApplicationSettings()
 gt.GitSync(repositoriesDict)
 
 if applicationSettings["job_schedule_frequency"] == Periodicity.MINUTE.value:
-    schedule.every(applicationSettings["job_schedule_factor"]).minutes.do(gt.GitSync,repositoriesDict)
+    schedule.every(applicationSettings["job_schedule_factor"]).minutes.do(
+        gt.GitSync, repositoriesDict)
 elif applicationSettings["job_schedule_frequency"] == Periodicity.HOURLY.value:
-    schedule.every(applicationSettings["job_schedule_factor"]).hours.do(gt.GitSync,repositoriesDict)
+    schedule.every(applicationSettings["job_schedule_factor"]).hours.do(
+        gt.GitSync, repositoriesDict)
 elif applicationSettings["job_schedule_frequency"] == Periodicity.DAILY.value:
-    schedule.every(applicationSettings["job_schedule_factor"]).days.do(gt.GitSync,repositoriesDict)
-
-
-# if applicationSettings["job_schedule_frequency"] == "minute":
-#     schedule.every(applicationSettings["job_schedule_factor"]).minutes.do(gt.GitSync(repositoriesDict))
+    schedule.every(applicationSettings["job_schedule_factor"]).days.do(
+        gt.GitSync, repositoriesDict)
 
 while True:
     schedule.run_pending()
